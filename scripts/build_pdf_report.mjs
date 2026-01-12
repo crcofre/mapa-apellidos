3bda99929be470ab786532f16f09167556002122
crcofre <contacto@cristiancofre.cl>
2026-01-12 17:12:13 -0300

    Update build_pdf_report.mjs

--- scripts/build_pdf_report.mjs
+++ scripts/build_pdf_report.mjs
@@ -495,12 +495,11 @@ async function buildHtmlReport({ slug, summary }) {
     .t-comunas col.c4{ width:42%; }
     .t-comunas col.c5{ width:12%; }
 
-    .card.below{
-  margin-top: 4mm;
+
 
   /* AJUSTE DE ANCHO PARA ALINEAR CON TABLAS SUPERIORES */
-  width: calc(100% + 4mm);
-  margin-right: -4mm;
+  /*width: calc(100% + 4mm);
+  /*margin-right: -4mm;
 }
 
     .foot{ font-size:11px; color:#666; margin-top:3mm; }
